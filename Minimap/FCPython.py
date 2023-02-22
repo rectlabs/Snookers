cf90676aa4e621262c09fc0ae7b3b24897e30cda
elishatofunmi <elishatofunmi@gmail.com>
2023-02-22 01:14:23 +0100

    check

--- Minimap/FCPython.py
+++ Minimap/FCPython.py
@@ -31,10 +31,11 @@ def createPitch(length,width, unity,linecolor): # in meters
             ax=fig.add_subplot(1,1,1)
            
             #Pitch Outline & Centre Line
-            plt.plot([0,0],[0,width], color=linecolor)
+            arcDiam = 5
+            plt.plot([0,arcDiam],[0,width-arcDiam], color=linecolor)
             plt.plot([0,length],[width,width], color=linecolor)
             plt.plot([length,length],[width,0], color=linecolor)
-            plt.plot([length,0],[0,0], color=linecolor)
+            plt.plot([length-arcDiam,0],[arcDiam,0], color=linecolor)
             plt.plot([length/5,length/5], [0,width], color = linecolor)
 
            

d408794fe723ed859e791158f654bfd488c449fc
elishatofunmi <elishatofunmi@gmail.com>
2023-02-22 01:45:48 +0100

    review theta1 and theta2

--- Minimap/FCPython.py
+++ Minimap/FCPython.py
@@ -40,10 +40,10 @@ def createPitch(length,width, unity,linecolor): # in meters
 
            
             # #Prepare Arcs
-            leftArcDown = Arc((0,0),height=archDiam*2,width=archDiam*2,angle=90,theta1 = 0, theta2=90, color=linecolor)
-            leftArcUp = Arc((0,width),height=archDiam*2,width=archDiam*2,angle=90, theta1 = 270, theta2 = 360, color=linecolor)
-            rightArcUp = Arc((length, width),height=archDiam*2,width=archDiam*2,angle=90, theta1 = 180, theta2 = 270, color=linecolor)
-            rightArcDown = Arc((length, 0), height=archDiam*2, width=archDiam*2, angle=90, theta1 = 90,theta2 = 180, color=linecolor)
+            leftArcDown = Arc((0,0),height=archDiam*2,width=archDiam*2,angle=90,theta1 = 90, theta2=180, color=linecolor)
+            leftArcUp = Arc((0,width),height=archDiam*2,width=archDiam*2,angle=90, theta1 = 180, theta2 = 270, color=linecolor)
+            rightArcUp = Arc((length, width),height=archDiam*2,width=archDiam*2,angle=90, theta1 = 270, theta2 = 360, color=linecolor)
+            rightArcDown = Arc((length, 0), height=archDiam*2, width=archDiam*2, angle=90, theta1 = 0,theta2 = 90, color=linecolor)
             
             # #Draw Arcs
             ax.add_patch(leftArcDown)

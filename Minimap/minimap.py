44b4e6d4543d477b39f3e7be4eec840026cc679f
elishatofunmi <elishatofunmi@gmail.com>
2023-02-24 13:02:19 +0100

    reeval

--- Minimap/minimap.py
+++ Minimap/minimap.py
@@ -19,9 +19,9 @@ class Minimap:
         ann = annotate()
         PCircle = []
         diameter = 2
-        positions = [(80,35), (84,33), (84,37), (88, 31), (88, 35), (88, 39), (92, 29), (92, 33), (92, 37), (92, 41), (96, 27), (96, 31), (96, 35), (96, 39), (96, 41)]
+        positions = [(80,35), (84,33), (84,37), (88, 31), (88, 35), (88, 39), (92, 29), (92, 33), (92, 37), (92, 41), (96, 27), (96, 31), (96, 35), (96, 39), (96, 43)]
         for (x,y) in positions:
-            passCircle = ann.CreateIdentity(x, y, str(np.random.choice([i for i in range(30)])/30), diameter = diameter)
+            passCircle = ann.CreateIdentity(x, y, str(np.random.choice([i for i in range(10)])/10), diameter = diameter)
             PCircle.append(passCircle)
         return PCircle
 

dd313d5106cff23c80719c3441f7f174bf65db7c
elishatofunmi <elishatofunmi@gmail.com>
2023-02-24 12:42:23 +0100

    random colors

--- Minimap/minimap.py
+++ Minimap/minimap.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from .soccer_module import SoccerModule, annotate
 
 def randomColor():
-    return np.random.choice([i for i in range(0,255)])
+    return np.random.choice([i for i in range(100,255)])
 
 class Minimap:
     def __init__(self,TeamA = 'TeamA', TeamB = 'TeamB', ScoreA = 0, ScoreB = 0):
@@ -21,8 +21,7 @@ class Minimap:
         diameter = 2
         positions = [(80,35), (82,34), (82,36), (84, 33), (84, 35), (84, 37), (86, 32), (86, 34), (86, 36), (86, 38), (88, 31), (88, 33), (88, 35), (88, 37), (88, 39)]
         for (x,y) in positions:
-            color = (randomColor(), randomColor(), randomColor())
-            passCircle = ann.CreateIdentity(x, y, color, diameter = diameter)
+            passCircle = ann.CreateIdentity(x, y, str(np.random.choice([i for i in range(30)])/30), diameter = diameter)
             PCircle.append(passCircle)
         return PCircle
 
